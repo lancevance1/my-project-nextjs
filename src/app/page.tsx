822a4247c1e098be8e892916bb1583350fbd7c68
Wenbo Xiao <xiaowenbo2008@gmail.com>
2023-08-13 18:15:43 +0930

    set correc version prettier and reformat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
-    return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    )
-  }
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+    </h1>
+  );
+}
