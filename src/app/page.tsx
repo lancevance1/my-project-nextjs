2f6533e7d0477e8fff4560117fd3eff2eb118aa6
Wenbo Xiao <xiaowenbo2008@gmail.com>
2023-08-13 19:09:44 +0930

    add .prettierignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+      <button className="bg-sky-700 px-4 py-2 text-white  hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
     </h1>
   );
 }
